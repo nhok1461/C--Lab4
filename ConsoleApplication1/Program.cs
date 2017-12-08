9dfab38ad06a1ce9f4f0e084d3630f65eec88c2d
AnKhang <nhok1461@gmail.com>
2017-12-08 10:22:21 +0700

    asdasdasdasdxcz

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -11,7 +11,6 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             //change code
-            //faskjgfkjasfkjdgafkjgskljfhjaklfghlauisirpfs hisvfsiybb toig;lock 
-        }
+            Console.WriteLine();
     }
 }
