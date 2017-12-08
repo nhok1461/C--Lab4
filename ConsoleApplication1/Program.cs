36a2944557cabd6e7c2aa217d7bbc6e3ac81431d
AnKhang <nhok1461@gmail.com>
2017-12-08 10:00:39 +0700

    dasfashvfhjasgrhfjakjgfsebthui3ashyeuisy5rueisrt904sy

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,6 +10,8 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            //change code
+            //faskjgfkjasfkjdgafkjgskljfhjaklfghlauisirpfs hisvfsiybb toig;lock 
         }
     }
 }
